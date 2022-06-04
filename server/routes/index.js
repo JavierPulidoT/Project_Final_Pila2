dd2dc0a18d5337503522ff923810556b5d30a881
JavierPulidoT <javierp.pulido@gmail.com>
2022-06-03 23:49:18 -0500

    Modern JS

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router();//creo instancia de un enrutador,
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
- let emojieDataset = ['💻'];
- let emojie = emojieDataset[Math.floor(Math.random() *
- emojieDataset.length)];
- res.render('index', { 
-   title: 'Express',
-   author:'🙍‍♂️Javier Pulido',
-   emojie
-  });
-
-
+router.get('/', function(req, res, next) { 
+  //render(callback) manda a llamar-renderizar al temple engine (generar y entregar )
+  //la vista al 
+  //calculando
+  let emojieDataset = ['💻','🔍','💸'];
+  let emojie = emojieDataset[Math.floor(Math.random() *
+  emojieDataset.length)];
+  res.render('index', { 
+    title: 'Express',
+    author:'🙍‍♂️Javier Pulido',
+    emojie
+   });
 });
 
 module.exports = router;
