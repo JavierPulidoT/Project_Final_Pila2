7ee4e2a4ca9a6dec6a82a700f142b8d0884f4252
JavierPulidoT <javierp.pulido@gmail.com>
2022-06-03 22:49:23 -0500

    Adding Babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ // res.render('index', { title: 'Express' });
+ let emojieDataset = ['💻'];
+ let emojie = emojieDataset[Math.floor(Math.random() *
+ emojieDataset.length)];
+ res.render('index', { 
+   title: 'Express',
+   author:'🙍‍♂️Javier Pulido',
+   emojie
+  });
+
+
 });
 
 module.exports = router;
