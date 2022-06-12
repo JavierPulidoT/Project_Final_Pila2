65de3c943c84046741ece7cdf42b348d28a3d3cd
JavierPulidoT <javierp.pulido@gmail.com>
2022-06-12 13:51:36 -0500

    S28 AirBnB Config

--- server/routes/about.js
+++ server/routes/about.js
@@ -1,16 +1,20 @@
+const express = require('express');
 
-var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   // render manda a renderizar (generar y entregar)
   // la vista al cliente
   // Este es el View-Model
-  res.render('about', 
-  { name: 'Javier Pulido', 
-    email: 'javierp.pulido@gmail.com',
-    url: "www.JavierPulido.com" });
+  res.render(
+    'about',
+    {
+      name: 'Javier Pulido',
+      email: 'javierp.pulido@gmail.com',
+      url: 'www.JavierPulido.com',
+    },
+  );
 });
 
 module.exports = router;
